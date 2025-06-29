0c053fdea273f393826f44ee3f8bd031618518e3
Sujay1512 <bharatinnovationconnect@gmail.com>
2025-06-29 20:25:24 +0530

    Updated Events.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ import {
   Home,
   GraduationCap,
   ExternalLink,
-  Calendar,
+  Trophy,
   Newspaper
 } from 'lucide-react';
 
@@ -24,7 +24,7 @@ const Header: React.FC = () => {
     { name: 'Dashboard', href: '/dashboard', icon: Home },
     { name: 'Courses', href: '/courses', icon: GraduationCap },
     { name: 'Resources', href: '/resources', icon: ExternalLink },
-    { name: 'Events', href: '/events', icon: Calendar },
+    { name: 'Hackathons', href: '/events', icon: Trophy },
     { name: 'News', href: '/news', icon: Newspaper },
   ];
 
